ba99632701bcc1e0737c25ac32172d74cef4d445
Segedu <seggedu20@gmail.com>
2022-02-08 13:21:05 +0200

    video player

--- server/client/src/screens/VideoPlayer/VideoPlayer.jsx
+++ server/client/src/screens/VideoPlayer/VideoPlayer.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './VideoPlayer.module.css';
+import style from './VideoPlayer.module.css';
 
 function VideoPlayer({ movieToPlay }) {
     return <div>
-        <iframe src={movieToPlay.video} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-    </div>;
+        <iframe className={style.videoPlayer} src={movieToPlay.video} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    </div>
 }
 
 export default VideoPlayer;
